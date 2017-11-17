9df8d0d6a63201c752f6a75268a7f0673a79f417
Tiwalayo Eisape <eisape@bc.edu>
2017-11-17 16:42:26 -0500

    moo!

--- parser.py
+++ parser.py
@@ -1,7 +1,10 @@
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import wordnet_ic
-
+from nltk.tag import pos_tag_sents
 brown_ic = wordnet_ic.ic('ic-brown.dat')
 
-#rhyming
+moneymitch = open('music/moneymitch.txt')
+for i in moneymitch.readlines():
+    print i
+
