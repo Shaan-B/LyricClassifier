3c91d804a9e2abd1bb6092364df4f3f98c79bf59
Tiwa Eisape <eisape@cscigpu02.bc.edu>
2017-12-06 21:04:07 -0500

    log files

--- workspace.py
+++ workspace.py
@@ -40,7 +40,7 @@ def oneHotEncoder(songs):
 
 g = ['blues', 'country', 'pop', 'rock', 'rap', 'r&b']
 
-songs = loadsongs.load('data/larkin1000_v2', g)
+songs = loadsongs.load('data/larkin1000_v2', g)#_allgenres', g)
 songs = [song for song in songs if song.genres[0] != 'rock']
 def genreDistribution(songs):
     print('Total number of songs:', len(songs))
@@ -74,26 +74,27 @@ posData = vectorize(lyrics2POS([song.simpleLyrics() for song in songs]), 3)
 poss = np.array(vectorize(lyrics2POS([song.simpleLyrics() for song in songs]), 3))
 labels = np.array(oneHotEncoder(songs))
 
-#newTensors = np.array([np.append(tfidfs[i], poss[i]) for i in range(len(tfidfs))])
+newTensors = np.array([np.append(tfidfs[i], poss[i]) for i in range(len(tfidfs))])
 #model = modelBuilder(len(newTensors.tolist()[0]), 10)
 #model.fit(newTensors, labels, n_epoch=100, show_metric=True, batch_size=25)
-model = modelcnn(len(poss.tolist()[0]), 10)
-model.fit(poss, np.array(oneHotEncoder(songs)), n_epoch=1000, batch_size=50, show_metric=True)
-posmodel, posencoder = autoEncoder(len(posData.tolist()[0]), 10)
-posmodel = trainEncoder(poss, posmodel, posencoder)
-possencodings = [posencoder.predict(np.array([pos])).tolist()[0] for pos in poss]
+#model = modelBuilder(len(poss.tolist()[0]), 10)
+#model.fit(poss, np.array(oneHotEncoder(songs)), n_epoch=1000, batch_size=50, show_metric=True)
+#posmodel, posencoder = autoEncoder(len(posData.tolist()[0]), 10)
+#posmodel = trainEncoder(poss, posmodel, posencoder)
+#possencodings = [posencoder.predict(np.array([pos])).tolist()[0] for pos in poss]
 
-tf.reset_default_graph()
+#tf.reset_default_graph()
 tfidfmodel, tfidfencoder = autoEncoder(len(data.tolist()[0]), 10)
 tfidfmodel = trainEncoder(tfidfs, tfidfmodel, tfidfencoder)
 tfidfencodings = [tfidfencoder.predict(np.array([tfidf])).tolist()[0] for tfidf in tfidfs]
 #trainX, trainY = np.array(data[:int(len(data) * .66)]), np.array(oneHotEncoder(songs)[:int(len(data) * .66)])
 #testX, testY = np.array(data[int(len(data) * .66):]), np.array(oneHotEncoder(songs)[int(len(data) * .66):])
-newTensors = [np.append(tfidfencodings[i], (possencodings[i])) for i in range(len(tfidfencodings))]
+#newTensors = [np.append(tfidfencodings[i], (possencodings[i])) for i in range(len(tfidfencodings))]
 
 tf.reset_default_graph()
-model = modelBuilder(len(tfidfencodings[1]) + len(possencodings[0]), 10)
-model.fit(np.array(newTensors), labels, n_epoch=100, show_metric=True, batch_size=25)
+#model = modelBuilder(len(tfidfencodings[1]) + len(possencodings[0]), 10)
+model = modelBuilder(len(tfidfencodings[1]), 10)
+model.fit(np.array(tfidfencodings), labels, n_epoch=100, show_metric=True, batch_size=25)
 
 
 tf.reset_default_graph()
