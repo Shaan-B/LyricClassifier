edbebf46737579e01ad35e3b69e8a75bf168ebca
Tiwa Eisape <eisape@cscigpu02.bc.edu>
2017-12-06 22:52:55 -0500

    logs

--- workspace.py
+++ workspace.py
@@ -54,6 +54,7 @@ def genreDistribution(songs):
             genrecountcounts[len(song.genres)] = 1
         for genre in song.genres:
             if genre in genrecounts.keys():
+
                 genrecounts[genre] += 1
             else:
                 genrecounts[genre] = 1
@@ -66,27 +67,34 @@ def genreDistribution(songs):
 
 
 print(genreDistribution(songs))
+
 data = vectorize([song.simpleLyrics() for song in songs], 1)
+
 #print data[0].tolist()
 
 tfidfs = np.array(data)
-posData = vectorize(lyrics2POS([song.simpleLyrics() for song in songs]), 3)
-poss = np.array(vectorize(lyrics2POS([song.simpleLyrics() for song in songs]), 3))
+#posData = vectorize(lyrics2POS([song.simpleLyrics() for song in songs]), 3)
+#poss = np.array(vectorize(lyrics2POS([song.simpleLyrics() for song in songs]), 3))
 labels = np.array(oneHotEncoder(songs))
+for i,_ in enumerate(songs):
+    if _.title == 'Homegirl': homeGirl = i
+print i
 
-newTensors = np.array([np.append(tfidfs[i], poss[i]) for i in range(len(tfidfs))])
+
+#newTensors = np.array([np.append(tfidfs[i], poss[i]) for i in range(len(tfidfs))])
 #model = modelBuilder(len(newTensors.tolist()[0]), 10)
 #model.fit(newTensors, labels, n_epoch=100, show_metric=True, batch_size=25)
-#model = modelBuilder(len(poss.tolist()[0]), 10)
-#model.fit(poss, np.array(oneHotEncoder(songs)), n_epoch=1000, batch_size=50, show_metric=True)
+model = modelBuilder(len(tfidfs.tolist()[0]), 10)
+model.fit(tfidfs, np.array(oneHotEncoder(songs)), n_epoch=20, batch_size=50, show_metric=True)
+print model.predict(np.array([tfidfs.tolist()[i]]))
 #posmodel, posencoder = autoEncoder(len(posData.tolist()[0]), 10)
 #posmodel = trainEncoder(poss, posmodel, posencoder)
 #possencodings = [posencoder.predict(np.array([pos])).tolist()[0] for pos in poss]
 
 #tf.reset_default_graph()
-tfidfmodel, tfidfencoder = autoEncoder(len(data.tolist()[0]), 10)
-tfidfmodel = trainEncoder(tfidfs, tfidfmodel, tfidfencoder)
-tfidfencodings = [tfidfencoder.predict(np.array([tfidf])).tolist()[0] for tfidf in tfidfs]
+#tfidfmodel, tfidfencoder = autoEncoder(len(data.tolist()[0]), 10)
+#tfidfmodel = trainEncoder(tfidfs, tfidfmodel, tfidfencoder)
+#tfidfencodings = [tfidfencoder.predict(np.array([tfidf])).tolist()[0] for tfidf in tfidfs]
 #trainX, trainY = np.array(data[:int(len(data) * .66)]), np.array(oneHotEncoder(songs)[:int(len(data) * .66)])
 #testX, testY = np.array(data[int(len(data) * .66):]), np.array(oneHotEncoder(songs)[int(len(data) * .66):])
 #newTensors = [np.append(tfidfencodings[i], (possencodings[i])) for i in range(len(tfidfencodings))]
@@ -94,7 +102,7 @@ tfidfencodings = [tfidfencoder.predict(np.array([tfidf])).tolist()[0] for tfidf 
 tf.reset_default_graph()
 #model = modelBuilder(len(tfidfencodings[1]) + len(possencodings[0]), 10)
 model = modelBuilder(len(tfidfencodings[1]), 10)
-model.fit(np.array(tfidfencodings), labels, n_epoch=100, show_metric=True, batch_size=25)
+model.fit(np.array(tfidfencodings), labels, n_epoch=20, show_metric=True, batch_size=25)
 
 
 tf.reset_default_graph()
